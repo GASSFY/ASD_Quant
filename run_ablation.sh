c1ee6497c8a8a9a102fd507c00d6b1f01017bc3b
HMZ <hwy1946725241@126.com>
2026-03-15 17:26:05 +0800

    feat：增加了消融实验的组数

--- run_ablation.sh
+++ run_ablation.sh
@@ -21,8 +21,13 @@ fi
 # ---------- Experiment configurations ----------
 # Format: "theta1 theta2 ratio"
 EXPERIMENTS=(
-    "1.0  0.0  0.01"
+    "0.0  1.0  0.01"
+    "0.1  0.9  0.01"
+    "0.3  0.7  0.01"
     "0.5  0.5  0.01"
+    "0.7  0.3  0.01"
+    "0.9  0.1  0.01"
+    "1.0  0.0  0.01"
 )
 
 NUM_EXPERIMENTS=${#EXPERIMENTS[@]}
